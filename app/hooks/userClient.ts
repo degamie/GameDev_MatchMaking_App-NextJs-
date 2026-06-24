fb717f620b36e480212868b28bc5e462e556b9a1
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-24 21:54:10 +0530

    Add files via upload

---
+++ app/hooks/userClient.ts
@@ -0,0 +1,22 @@
+//WID(24.06.2026(Sarthak Mittal)DegamieSIgn(UserClient Hook Impl))
+// import { useCreateChatClient } from "stream-chat-react";
+// import { createToken } from "../../../actions/stream.action";
+import { useCallback } from "react";
+// import type { Leaderboard } from "../../../types";
+export const userClient=(
+    user:LeaderBoard
+    )=>{
+    	const tokenProvider = useCallback(async () => {
+		return await createToken(user);
+	}, [user]);
+
+	const client = useCreateChatClient({
+		apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY!,
+		tokenOrProvider: tokenProvider,
+		userData: { id: user.id, name: user.nickname, image: `https://api.dicebear.com/9.x/pixel-art/svg?seed=${user.nickname}` },
+	});
+
+	if (!client) return { client: null };
+
+	return { client };
+};
