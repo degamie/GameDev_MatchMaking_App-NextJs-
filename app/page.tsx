b4d0d27561acf7bac77a809d7d538a2b3e8a6fb2
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-25 21:52:29 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,139 @@
+//WID(25/6/2026)(Sarthak Mittal(DegamieSign)(LoginPage))
+"use client";
 import Image from "next/image";
 import GameCreate from "./GameCreate";
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <GameCreate/>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+
+import { loginUser } from "@/lib/serverfunctions";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+import Link from "next/link";
+export default function Login() {
+  const router=useRouter();
+  const[btnClicked,setbtnClicked]=useState<boolean>(false);
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setbtnClicked(true);
+		const form = e.currentTarget;
+		const formData = new FormData(form);
+		const { user, message } = await loginUser(formData)
+		if (user) {
+			toast.success("Sign in successful! 🎉", {
+				description: message
+			})
+			router.push("/games");
+		} else {
+			toast.error("Authentication Failed", {
+				description: message
+			})
+			setbtnClicked(false);
+		}
+	};
+
+	return (
+		<section className='mx-auto md:w-3/4 w-full h-screen flex flex-col justify-center md:px-8 px-6 items-center'>
+			<h2 className='text-3xl font-bold mb-3 md:text-left text-center'>
+				Log in
+			</h2>
+			<form className='w-full' onSubmit={handleSubmit}>
+				<label htmlFor='email' className='mb-2 opacity-60'>
+					Email Address
+				</label>
+				<input
+					required
+					type='email'
+					id='email'
+					name='email'
+					className='w-full px-4 py-3 border-[1px] rounded-md mb-3'
+				/>
+
+				<label htmlFor='password' className='mb-2 opacity-60'>
+					Password
+				</label>
+				<input
+					required
+					type='password'
+					id='password'
+					name='password'
+					className='w-full px-4 py-3 border-[1px] rounded-md mb-2'
+				/>
+
+				<button
+					type='submit'
+					className='mt-6 mb-2 text-lg text-blue rounded-md bg-purple-500 w-full px-8 py-4 cursor-pointer hover:bg-orange-600'
+					disabled={btnClicked}
+				>
+					{btnClicked ? "Signing in" : "Sign in"}
+				</button>
+				<p className=' opacity-60 text-center'>
+					Don&apos;t have an account?{" "}
+					<Link href='/register' className='text-blue-800'>
+						Create one
+					</Link>
+				</p>
+			</form>
+		</section>
+	);
 }
+
+    // <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    //   <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    //     <Image
+    //       className="dark:invert"
+    //       src="/next.svg"
+    //       alt="Next.js logo"
+    //       width={100}
+    //       height={20}
+    //       priority
+    //     />
+    //     <GameCreate/>
+    //     <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+    //       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+    //         To get started, edit the page.tsx file.
+    //       </h1>
+    //       <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+    //         Looking for a starting point or more instructions? Head over to{" "}
+    //         <a
+    //           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    //           className="font-medium text-zinc-950 dark:text-zinc-50"
+    //         >
+    //           Templates
+    //         </a>{" "}
+    //         or the{" "}
+    //         <a
+    //           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    //           className="font-medium text-zinc-950 dark:text-zinc-50"
+    //         >
+    //           Learning
+    //         </a>{" "}
+    //         center.
+    //       </p>
+    //     </div>
+    //     <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+    //       <a
+    //         className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <Image
+    //           className="dark:invert"
+    //           src="/vercel.svg"
+    //           alt="Vercel logomark"
+    //           width={16}
+    //           height={16}
+    //         />
+    //         Deploy Now
+    //       </a>
+    //       <a
+    //         className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Documentation
+    //       </a>
+    //     </div>
+    //   </main>
+    // </div>
