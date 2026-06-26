f4d9003b84dffabe011ee0c0aaec3d74c084e9da
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-26 12:53:37 +0530

    Add files via upload

---
+++ app/components/Chat/ChatComponent.tsx
@@ -0,0 +1,58 @@
+//WID(26/6/2026)(Sarthak Mittal)
+"useClient";
+import React from 'react'
+// import {
+// 	Channel,
+// 	ChannelList,
+// 	Window,
+// 	ChannelHeader,
+// 	MessageList,
+// 	MessageInput,
+// } from "stream-chat-react";
+
+// import { Loader2 } from "lucide-react";
+// import { Chat } from "stream-chat-react";
+
+export default  function ChatComponent(user:{user:Leaderboard}){
+ 	const filters = {
+		type: "messaging",
+		id: { $in: ["tic-tac-toe", "rock-paper-scissors"] },
+		members: { $in: [user.id] },
+	};
+
+	const options = { presence: true, state: true };
+
+	if (!client)
+		return (
+			<div className='flex items-center justify-center h-screen'>
+				<Loader2 className='animate-spin text-blue-500' />
+			</div>
+		);
+
+	return (
+		<Chat client={client}>
+			<div className='chat-container'>
+				{/* -- Channel List -- */}
+				<div className='channel-list'>
+					<ChannelList
+						sort={{ last_message_at: -1 }}
+						filters={filters}
+						options={options}
+					/>
+				</div>
+
+				{/* -- Messages Panel -- */}
+				<div className='chat-panel'>
+					<Channel>
+						<Window>
+							<ChannelHeader />
+							<MessageList />
+							<MessageInput />
+						</Window>
+					</Channel>
+				</div>
+			</div>
+		</Chat>
+	);
+}
+
