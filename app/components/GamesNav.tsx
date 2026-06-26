39569252ed9caf674f05eca074fe473c02bac92b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-26 15:36:13 +0530

    Add files via upload

---
+++ app/components/GamesNav.tsx
@@ -0,0 +1,8 @@
+//WID(26/6/2026)(Sarthak Mittal(DegamieSign(GamesNav)))
+import React from 'react'
+import Link from "next/link";
+export default function GamesNav() {
+  return (
+    <div>GamesNav</div>
+  )
+}
