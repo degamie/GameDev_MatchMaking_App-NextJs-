9f5f3d67d1c0f4c66cb9144161f86bd9d2c4b9bc
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-28 11:24:57 +0530

    Add files via upload

--- app/GameCreate.tsx
+++ app/GameCreate.tsx
@@ -1,5 +1,6 @@
-//WID(24/6/2026)(Sarthak Mittal(DegamieSign(GameCreate)
+//WID(26/6/2026)(Sarthak Mittal(DegamieSign(GameCreate)#1
 export default function GameCreate(){
+	const [gametype,setgametype]=useState<string>("type");
     const[selectedGame,setSelectedGame]=useState<string>("");
     const [buttonClicked, setButtonClicked] = useState<boolean>(false);
     	const [selectedPlayer, setSelectedPlayer] = useState<string>("");
@@ -7,4 +8,8 @@ export default function GameCreate(){
     	const [error, setError] = useState<boolean>(true);
     	const router = useRouter();
     	const handleCopy = () => setCopied(true);
+		const handleSubmit=async (e:React.FormEvent<HTMLFormElement>)=>{
+			e.preventDefault();
+			setButtonClicked(true);	
+		}
 }
