cfdd1160085a68b4dff34bc46f8f6a5b5a7975b3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-06-28 15:21:26 +0530

    Add files via upload

---
+++ app/components/gameslug/page/gameslug_page.tsx
@@ -0,0 +1,12 @@
+//WID(28/6/2026)(Sarthak Mittal(DegamieSign)(GamePageProps))
+import React from 'react'
+interface GamePageprops{
+     params:Promise<{gameslug:string}>;
+}
+function gameslug_page() {
+  return (
+    <div>gameslug_page</div>
+  )
+}
+
+export default gameslug_page
