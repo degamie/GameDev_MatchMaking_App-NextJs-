c72d0e2ed35e14b6e2ccb0c450769a7d9cca6cd3
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-07-01 10:08:04 +0530

    Add files via upload

--- app/components/GamesNav.tsx
+++ app/components/GamesNav.tsx
@@ -1,9 +1,10 @@
 //WID(27/6/2026)(Sarthak Mittal(DegamieSign(GamesNav)))
 import React from 'react'
 import Link from "next/link";
-export default function GamesNav() {
+export default async  function GamesNav() {
+  const gameapp=await getPublishGameList();
   return (
     <><div>GamesNav</div><Link href='' className='font-bold-text-2xl'> Game Streaming</Link></>
 
   )
-}
+} 
