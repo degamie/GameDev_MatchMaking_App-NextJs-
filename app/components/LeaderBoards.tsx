95b5974e1d055c1514cf57013b9d37f21033074c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-07-02 19:00:40 +0530

    Add files via upload

---
+++ app/components/LeaderBoards.tsx
@@ -0,0 +1,9 @@
+//WID(2/7/2026)(Sarthak Mittal(DegameiSign(leaderBoards)))_impl
+import React from 'react'
+ export default function LeaderBoards(){ 
+    const[topScore,settopscore]=useState(true);
+  return (
+    <div>LeaderBoards</div>
+  )
+}
+
