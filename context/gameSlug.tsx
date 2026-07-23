32b663ed494d084a6324174e5381160a4c8060c2
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-07-23 10:37:01 +0530

    Add files via upload

---
+++ context/gameSlug.tsx
@@ -0,0 +1,28 @@
+//WID(23/7/2026)(Sarthak Mittal(DegamieSign)(GamePageProps))#1.1
+"use-client"
+import React, { useState } from 'react'
+import { Suspense } from 'react';
+import StreamGridSkeleton from '../../StreamGridSkeleton';
+import LiveStreamsList from '../../LiveStreamsList';
+// import StreamGridSkeleton from '@/components/StreamGridSkeleton';
+// import LiveStreamsList from '@/components/LiveStreamsList';
+interface GamePageprops{
+     params:Promise<{gameslug:string}>;
+}
+  setgameid=(gameid)=>{this.gameId=gameid;}//GameId's Instantiate
+export default  async function GameSlug({params}: { params: Promise<{ gameSlug: string }> }) { 
+
+  const[gameid,setgameid]=useState(0);
+  const [gameSlug,setgameslug]=useState(1);
+  const {gameSlug}=await params;
+return (
+    <div>
+      <h2 className="text-xl font-semibold mb-4">Active Broadcasts</h2>
+      
+      {/* The layout renders instantly, and the stream grid hydrates when ready */}
+      <Suspense fallback={<StreamGridSkeleton />}>
+        <LiveStreamsList gameSlug={gameSlug} />
+      </Suspense>
+    </div>
+  );
+}
