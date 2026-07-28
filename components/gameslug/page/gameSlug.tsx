d5822110261aa918e72f7bddd7aabac505718115
Degamie_sign <degamiesign@gmail.com>
2026-07-28 20:08:06 +0530

    GameSlugName_HooksTate_Instantiate

--- components/gameslug/page/gameSlug.tsx
+++ components/gameslug/page/gameSlug.tsx
@@ -1,4 +1,4 @@
-//WID(28/7/2026)(Sarthak Mittal(DegamieSign)(GamePageProps))#1.1
+//WID(28/7/2026)(Sarthak Mittal(DegamieSign)(GamePageProps))#1.1/1s
 "use-client"
 import React, { useState } from 'react'
 import { Suspense } from 'react';
@@ -9,6 +9,7 @@ import LiveStreamsList from '../../LiveStreamsList';
 interface GamePageprops{
      params:Promise<{gameslug:string}>;
 }
+const[gameslugname,setgameslugname]=useRef(()=>{gameslugname});
 constructor(GameSlug:string,gameid:string, GameSlugname:string){
   this.gameSlug=GameSlug;
 }
