29149ac60933af25c91cb9e27678542e84cc3eeb
Degamie_sign <degamiesign@gmail.com>
2026-07-31 09:29:13 +0530

    pagename#instantiate#hookstate

--- components/gameslug/page/[Streaming]/Streampage.tsx
+++ components/gameslug/page/[Streaming]/Streampage.tsx
@@ -1,8 +1,9 @@
-//WID(26/7/2026)(Sarthak Mittal(DegaimieSign(StreamPage)))x1#Impl
-import React, { Suspense } from "react"
+//WID(31/7/2026)(Sarthak Mittal(DegaimieSign(StreamPage)))x1#Impl
+import React, { Suspense, useState } from "react"
 
 setpageid=(pageid)=>{this.pageid=pageid;}//Instantiate pageid in GameApp
-export default function Streampage() { 
+export default function Streampage() {
+  const[pagename,setpagename]=useState(1); 
   const[pageid,setpageid]=useState(1);
   return (
     <>
