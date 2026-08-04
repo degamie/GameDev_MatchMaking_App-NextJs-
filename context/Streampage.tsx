a31a50849da34b9488dd6d4ac54b4edeef9c445f
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-04 11:39:40 +0530

    Add files via upload

--- context/Streampage.tsx
+++ context/Streampage.tsx
@@ -1,8 +1,10 @@
-//WID(26/7/2026)(Sarthak Mittal(DegaimieSign(StreamPage)))x1#Impl
-import React, { Suspense } from "react"
-
-setpageid=(pageid)=>{this.pageid=pageid;}//Instantiate pageid in GameApp
-export default function Streampage() { 
+//WID(4/8/2026)(Sarthak Mittal(DegaimieSign(StreamPage)))x1#Impl
+import React, { Suspense, useState } from "react"
+function setpagename(pagename){
+    this.paheName=pagename;}
+function setpageid(pageid){this.pageid=pageid;}//Instantiate pageid in GameApp
+export default function Streampage() {
+  const[pagename,setpagename]=useState(1); 
   const[pageid,setpageid]=useState(1);
   return (
     <>
