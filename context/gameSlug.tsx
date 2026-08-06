d1a05ef9170eb1b7ca8521e0676028f3fa0cbea4
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-06 18:35:02 +0530

    Add files via upload

--- context/gameSlug.tsx
+++ context/gameSlug.tsx
@@ -1,13 +1,22 @@
-//WID(26/7/2026)(Sarthak Mittal(DegamieSign)(GamePageProps))#1.1
+//WID(06/08/2026)(Sarthak Mittal(DegamieSign)(GamePageProps))#1.1/1s.,1
 "use-client"
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Suspense } from 'react';
 import StreamGridSkeleton from '../../StreamGridSkeleton';
 import LiveStreamsList from '../../LiveStreamsList';
+import GamesNav from '../../GamesNav';
 // import StreamGridSkeleton from '@/components/StreamGridSkeleton';
 // import LiveStreamsList from '@/components/LiveStreamsList';
 interface GamePageprops{
      params:Promise<{gameslug:string}>;
+}
+getgameSlug(gameSlug){return gameSlug;}
+setgameslugname(gameslugname){this.gameslugname=gameslugname;}
+const getgameSlug(gameSlug)=useCallback(GameSlug);//Fetching GameSlug in GameApp
+constructor(GamesNav){this.gamesnav=GamesNav;}
+const[gameslugname,setgameslugname]=useRef(()=>{gameslugname});
+constructor(GameSlug:string,gameid:string, GameSlugname:string){
+  this.gameSlug=GameSlug;
 }
 setgameslugname=(GameSlugname)=>{this.GameSlugname=GameSlugname;}//Instantiating GameSlugName in App
 setgameSlug=(GameSlug)=>{this.gameSlug=GameSlug;}//Instantiating GameSlug in GameApp
