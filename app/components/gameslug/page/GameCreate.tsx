c98f1edbf87960f027b06787f0e317f2dc2050be
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-09 17:45:38 +0530

    Add files via upload

---
+++ app/components/gameslug/page/GameCreate.tsx
@@ -0,0 +1,24 @@
+//WID(9/8/2026)(Sarthak Mittal(DegamieSign(GameCreate)#1.1
+import { useRouter } from "next/dist/client/router";
+import { useContext, useState } from "react";
+constructor(selectedGame:string){
+	this.gametype=gametype;
+		this.selectedGame=selectedGame;
+	}
+	setgametype(gametype){this.gametype=gametype;}
+
+export default function GameCreate(){
+	
+	const [gametype,setgametype]=useState<string>("type");
+    const[selectedGame,setSelectedGame]=useState<string>("");
+    const [buttonClicked, setButtonClicked] = useState<boolean>(false);
+    	const [selectedPlayer, setSelectedPlayer] = useState<string>("");
+    //	const {channel,setChannel } = useContext(ChannelContext);
+    	const [error, setError] = useState<boolean>(true);
+    	const router = useRouter();
+    //	const handleCopy = () => setCopied(true);
+		const handleSubmit=async (e:React.FormEvent<HTMLFormElement>)=>{
+			e.preventDefault();
+			setButtonClicked(true);	
+		}
+}
