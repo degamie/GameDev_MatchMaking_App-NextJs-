0405b39d857c7dc1879f48bc3843b8871465ef90
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-09 12:50:12 +0530

    Add files via upload

--- app/components/GamesNav.tsx
+++ app/components/GamesNav.tsx
@@ -1,12 +1,38 @@
-//WID(01/07/2026)(Sarthak Mittal(DegamieSign(GamesNav)))#1.1
-import React from 'react'
+//WID(9/8/2026)(Sarthak Mittal(DegamieSign(GamesNav)))#1.1.1(Impl)
+import React, { constructor, useCallback, useRef, useState } from 'react'
 import Link from "next/link";
+
+existsByGamesNav(GamesNav:Navigation){
+  if(GamesNav.length!==null)getGamesNav(GamesNav);
+  else getGamesNav(null);
+}
+updateByGamesNav(GamesNav:Navigation){
+  getGamesNav(GamesNav)+setGamesNav(gamenav)+1;
+}
+function setGamesNav(GamesNav:Navigation){this.gamesNav=gamenav;}
+getGamesNav(GamesNav){return GamesNav;}
+setrendercnt(rendercnt:number){this.rendercnt=rendercnt;}//Instantiate rendercnt in Game App.
+constructor(gameid,gamenav,gamename){
+      this.gameid=gameid;
+      this.gamenav=gamenav;
+    this.gamename=gamename;
+}
+ const setgamenav=(gamesNav)=>{this.gamesNav=gamenav;}
+
+const[gamerendercnt,setgamerendercnt]=useState(1);
+const[gamenav,setgamenav]=useCallback(GamesNav);
 export default async  function GamesNav() {
-  const [navigation,setnavigation]=useState(0);
-  const [navid,setnavid]=useRef(true);
-  const gameapp=await getPublishGameList();
-  return (
-    <><div>GamesNav</div><Link href='' className='font-bold-text-2xl'> Game Streaming</Link></>
+    setgameid=(gameId)=>{this.gameid=gameid;}
+  setgamename=(gamename)=>{this.gamename=gamename;}//Instantiating GameName 
+  const [gameid,setgameid]=useState(true);
+    return (
+       <><div>GamesNav</div><Link href='' className='font-bold-text-2xl'> Game Streaming</Link></>
 
   )
 } 
+  // const [navname,setnavname]=useState(true);
+  // const [navigation,setnavigation]=useState(0);
+  // const [navid,setnavid]=useRef(true);
+  // const gameapp=await getPublishGameList();
+
+   
