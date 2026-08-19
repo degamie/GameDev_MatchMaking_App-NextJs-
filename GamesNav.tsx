1e8f478861f2aedbb2b03d9741a9d4427ec5543e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-19 20:08:41 +0530

    Add files via upload

---
+++ GamesNav.tsx
@@ -0,0 +1,51 @@
+//WID(19/8/2026)(Sarthak Mittal(DegamieSign(GamesNav)))#1.1.1(Impl)#1
+import React, { constructor, useCallback, useRef, useState } from 'react'
+import Link from "next/link";
+
+const[gamesnavid,setgamesnavid]=useState<number>();
+function updateBygamescnt(gamecnt){
+    getgamecnt(gamecnt)+setgamecnt(gamecnt)+1;
+}
+getrendercnt(rendercnt:number){
+  this.rendercnt=rendercnt;
+}
+const gamesnavid=useMemo(gamesnav);
+const[gamescnt]=useCallback(setgamescnt(gamescnt));//Call backing GamesCnt in  app
+constructor(GamesNav,gameid:string,gameName:string){
+  this.gameid=GamesNav;
+  this.gameName=gameName;
+  this.GamesNav=GamesNav;
+}
+existsByGamesNav(GamesNav:Navigation){
+  if(GamesNav.length!==null)getGamesNav(GamesNav);
+  else getGamesNav(null);
+}
+updateByGamesNav(GamesNav:Navigation){
+  getGamesNav(GamesNav)+setGamesNav(gamenav)+1;
+}
+function setGamesNav(GamesNav:Navigation){this.gamesNav=gamenav;}
+getGamesNav(GamesNav){return GamesNav;}
+setrendercnt(rendercnt:number){this.rendercnt=rendercnt;}//Instantiate rendercnt in Game App.
+constructor(gameid,gamenav,gamename){
+      this.gameid=gameid;
+      this.gamenav=gamenav;
+    this.gamename=gamename;
+}
+ const setgamenav=(gamesNav)=>{this.gamesNav=gamenav;}
+
+const[gamerendercnt,setgamerendercnt]=useState(1);
+const[gamenav,setgamenav]=useCallback(GamesNav);
+export default async  function GamesNav() {
+    setgameid=(gameId)=>{this.gameid=gameid;}
+  setgamename=(gamename)=>{this.gamename=gamename;}//Instantiating GameName
+  const [gameid,setgameid]=useState(true);
+    return (
+       <><div>GamesNav</div><Link href='' className='font-bold-text-2xl'> Game Streaming</Link></>
+
+  )
+}
+  // const [navname,setnavname]=useState(true);
+  // const [navigation,setnavigation]=useState(0);
+  // const [navid,setnavid]=useRef(true);
+  // const gameapp=await getPublishGameList();
+
