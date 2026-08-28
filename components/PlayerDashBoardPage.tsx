ee6a57e1b7dde5b3e82e88aa2e2e12cc76ecd764
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-28 08:05:59 +0530

    Add files via upload

--- components/PlayerDashBoardPage.tsx
+++ components/PlayerDashBoardPage.tsx
@@ -1,11 +1,14 @@
-//WID(25/8/2026)(Sarthak Mittal(DegamieSign(PlayerDaShBoard(impl))))z#1/1it.1
+//WID(28/8/2026)(Sarthak Mittal(DegamieSign(PlayerDaShB oard(impl))))z#1/1it.1.1Cx` 
 import { redirect } from 'next/dist/server/api-utils';
-import React, {constructor, useCallback} from 'react'
+import React, {constructor, useCallback, useMemo} from 'react'
+function updateBycreateGame(createGame){
+    getcreateGame(createGame)+setcreateGame(createGame)+1;}
 setcreateGame(createGame){
     this.game = createGame;
 }
 const createGame=useCallback(PlayerDashBoardPage());
 export async function PlayerDashBoardPage(){
+    const setplayer=useMemo(this.player);
     constructor(player){
         this.player=player;
     }
