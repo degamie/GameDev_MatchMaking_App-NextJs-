dc5f7c6985703dfbccd727c3e061d48027429f09
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-31 20:41:57 +0530

    Add files via upload

---
+++ gameSlug.tsx
@@ -0,0 +1,64 @@
+//WID(31/8/2026)(Sarthak Mittal(DegamieSign(GameSlug(Param_constructor))))#1s#impl.1.1.1/1.1
+import React, {constructor, Suspense, useState} from 'react';
+import StreamGridSkeleton from '../../StreamGridSkeleton';
+import LiveStreamsList from '../../LiveStreamsList';
+import GamesNav from '../../GamesNav';
+function updateBygamescnt(gamescnt:GamesNav):void {
+    getgamescnt(gamescnt)+setgamescnt(gamescnt)+1;
+}
+function setgamescnt(gamescnt: GamesNav) {
+    this.gamescnt = gamescnt;
+}
+
+function getgamescnt(gamescnt: GamesNav) {
+    return gamescnt;
+}
+
+function setincrementgamescnt(gamesNum:number){
+    this.gamesNum =incrementgamesnym(gamesNum);
+}
+function incrementgamesnym(gamesNum: number){
+    gamesNum+=1;
+}
+function calculategamenum(gamenum:number) {
+    gamenum = Number(gamenum);
+}
+function setgamerendercnt(){this.gamerendercnt=0;}
+function getrendercnt(rendercnt:number){
+    return rendercnt;
+}
+function setgamesname(gamename:string){
+    this.gamename=gamename;
+}
+constructor(gameSlug:string){
+    this.gameSlug;
+}
+interface GamePageProps {
+  params: Promise<{ gameSlug: string }>;
+}
+function getgamepageid(gameid:string){
+    return gameid;
+}
+function setgamepageid(gameid:string){
+    this.gamesid=gameid;
+}
+const[gamespageid,setgamepageid]=useState(1);
+export default async function GamePage({ params }: GamePageProps) {
+  setgamesnav(gamesnav:GamesNav){this.gamesnav=gamesnav;}
+  const { gameSlug } = await params;
+  constructor(gameslug){
+    this.gameSlug=constructor;
+  }
+
+  return (
+    <div>
+      <GamesNav />
+      <h2 className="text-xl font-semibold mb-4">Active Broadcasts</h2>
+
+      {/* The layout renders instantly, and the stream grid hydrates when ready */}
+      <Suspense fallback={<StreamGridSkeleton />}>
+        <LiveStreamsList gameSlug={gameSlug} />
+      </Suspense>
+    </div>
+  );
+}
