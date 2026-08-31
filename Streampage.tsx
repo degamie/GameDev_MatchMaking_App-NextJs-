b90d17614284dffe9f8874c3108949defe03e227
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-08-31 12:11:02 +0530

    Add files via upload

---
+++ Streampage.tsx
@@ -0,0 +1,42 @@
+//WID(31/8/2026)(Sarthak Mittal(DegaimieSign(StreamPage)))x1#Impl#1t c
+import React, { Suspense, useEffect, useMemo, useState } from "react"
+function updatepagename(pagename){
+  getpagename(pagename)+setpagename(pagename)+1;
+
+  function getpagename(pagename) {
+    return pagename;
+  }
+
+
+}
+function setpagename(pagename){
+    this.paheName=pagename;}
+function setpageid(pageid){this.pageid=pageid;}//Instantiate pageid in GameApp
+export default function Streampage() {
+  const[pagename,setpagename]=useState(1); 
+  const[pageid,setpageid]=useState(1);
+  const computedVal=useMemo(()=>{
+       console.log("Executing GameDev's StreamPage");
+    return streamcnt+streamultiplier;
+  },[streamcnt]
+  useEffect(()=>{
+    console.log("Executing useEffect");
+  })
+  );
+
+  };
+  return (
+    <>
+    <VideoPlayer/>
+    <Suspense fallback={<ChatSkeleton/>}></Suspense>
+    <LiveChat>
+      <Suspense fallback={<RecommendationsSkeleton/>}>
+      <RecomendedStreams/>
+      </Suspense>
+
+    </LiveChat></>
+
+    // <div>Streampage</div>
+  )
+}
+
